bd366efa4cdebe956f66e101dd549494951d1ade
adminatpixelbloom <admin@pixelbloom.cz>
2025-08-31 23:34:22 +0200

    feat(Image): Add safe urls of picture to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent.cdninstagram.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "instagram.fprg6-1.fna.fbcdn.net",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
